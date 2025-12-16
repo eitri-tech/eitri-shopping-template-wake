4565037a80f060f0758f6a2e559fa8372d73222d
Leonardo <leonardo.silva@calindra.com.br>
2025-12-16 08:39:33 -0300

    Atualiza bifrost

--- shopping-wake-template-account/eitri-app.conf.js
+++ shopping-wake-template-account/eitri-app.conf.js
@@ -3,18 +3,17 @@ module.exports = {
 	'title': 'shopping-wake-template-account',
 	'slug': 'shopping-wake-template-account',
 	'eitri-luminus': '2.22.0',
-	'eitri-bifrost': '4.3.0',
-	'eitri-commons': '2.2.0',
-	'version': '1.0.2',
-	'versionMessage': 'add tela de Debug',
+	'eitri-bifrost': '4.4.0',
+	'version': '1.0.3',
+	'versionMessage': 'Atualiza bifrost',
 	'public-key': 'af3fe0cd-e730-4b6d-9a36-57852b1e05a6',
 	'applicationId': 'a8576e3a-2f34-4257-8843-b96d1038e37b',
 	'id': 'd2a1b412-cc94-4244-9982-882ce0370c43',
 	'organizationId': 'cf5660ee-bf90-42cd-9a43-9d2c69ee3c89',
 	'organization': {},
 	'eitri-app-dependencies': {
-		'eitri-shopping-wake-shared': { isEitriAppShared: true, version: '1.6.0' },
-		'shopping-wake-template-shared': { isEitriAppShared: true, version: '0.1.0' }
+		'eitri-shopping-wake-shared': { isEitriAppShared: true, version: '1.6.2' },
+		'shopping-wake-template-shared': { isEitriAppShared: true, version: '1.0.2' }
 	},
 	'type': 'module',
 	'check-js': false
