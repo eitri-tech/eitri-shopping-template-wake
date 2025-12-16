6999eff59c66182d81ede34466ed2fcb22513b37
Leonardo <leonardo.silva@calindra.com.br>
2025-12-16 08:21:25 -0300

    PDP 1.0.3 Atualiza bifrost

--- shopping-wake-template-pdp/eitri-app.conf.js
+++ shopping-wake-template-pdp/eitri-app.conf.js
@@ -3,18 +3,17 @@ module.exports = {
 	'title': 'shopping-wake-template-pdp',
 	'slug': 'shopping-wake-template-pdp',
 	'eitri-luminus': '2.8.0',
-	'eitri-bifrost': '3.12.2',
-	'eitri-commons': '2.2.0',
-	'version': '1.0.2',
-	'versionMessage': 'Atualiza luminus',
+	'eitri-bifrost': '4.4.0',
+	'version': '1.0.3',
+	'versionMessage': 'Atualiza bifrost',
 	'public-key': 'ea8a93ff-d8d1-4885-af87-3c45d46462cb',
 	'applicationId': 'a8576e3a-2f34-4257-8843-b96d1038e37b',
 	'id': '30cff1fe-30af-48ab-9021-4ea5ad4fbdb7',
 	'organizationId': 'cf5660ee-bf90-42cd-9a43-9d2c69ee3c89',
 	'organization': {},
 	'eitri-app-dependencies': {
-		'eitri-shopping-wake-shared': { isEitriAppShared: true, version: '1.4.9' },
-		'shopping-wake-template-shared': { isEitriAppShared: true, version: '0.1.0' }
+		'eitri-shopping-wake-shared': { isEitriAppShared: true, version: '1.6.0' },
+		'shopping-wake-template-shared': { isEitriAppShared: true, version: '1.0.2' }
 	},
 	'type': 'module',
 	'check-js': false
